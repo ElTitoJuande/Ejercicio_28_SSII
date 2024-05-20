989fbe8cc3656bfac8b5521747d429531ec3a937
psaenzguerrero <168193734+psaenzguerrero@users.noreply.github.com>
2024-05-20 17:33:22 +0200

    Update Ejercicio_28_SSII.java

--- src/ejercicio_28_ssii/Ejercicio_28_SSII.java
+++ src/ejercicio_28_ssii/Ejercicio_28_SSII.java
@@ -17,6 +17,7 @@ public class Ejercicio_28_SSII {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Me encanta mi profesor de Sistemas Informáticos");
+        System.out.println("Esto es del mejor compañero de mesa");
     }
     
 } 
